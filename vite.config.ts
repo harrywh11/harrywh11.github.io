c7bd44577d84a8465cc9bdedc17fec094cc36079
Harry Wang <harrywang11@126.com>
2025-02-24 19:41:07 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Replace "repo-name" with the actual name of your GitHub repository
-  base: '/harrywh11.github.io/',
 });
